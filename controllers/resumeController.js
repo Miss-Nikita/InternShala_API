8dc68f6cca6ae371acb04ab18452a4fa05f21a51
Miss-Nikita <bisennikita770@gmail.com>
2025-01-15 12:21:49 +0530

    Complete Education and Job Routes || Work on Intenships Route

--- controllers/resumeController.js
+++ controllers/resumeController.js
@@ -1,19 +1,82 @@
 const { catchAsyncErrors } = require("../middleware/catchAsyncErrors");
 const Student = require("../models/studentModel");
 
- const {v4: uuidv4} = require("uuid")
+const { v4: uuidv4 } = require("uuid");
 
-exports.resume = catchAsyncErrors(async(req,res,next)=>{
-    const {resume} = await Student.findById(req.id).exec();
-    res.json({message:"Secure Resume Page!",resume})
-})
-
-
-
-exports.addeducation = catchAsyncErrors(async(req,res,next)=>{
-    const student = await Student.findById(req.id).exec();
-    student.resume.education.push({...req.body, id:uuidv4});
-    await Student.save();
-    res.json({message:"Education Added!"})
+exports.resume = catchAsyncErrors(async (req, res, next) => {
+  const { resume } = await Student.findById(req.id).exec();
+  res.json({ message: "Secure Resume Page!", resume });
 });
- 
+
+// -----------------------Education------------------------------
+
+exports.addeducation = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  student.resume.education.push({ ...req.body, id: uuidv4() });
+  await student.save();
+  res.json({ message: "Education Added!" });
+});
+
+exports.editeducation = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const eduIndex = student.resume.education.findIndex(
+    (i) => i.id === req.params.eduid
+  );
+  student.resume.education[eduIndex] = {
+    ...student.resume.education[eduIndex],
+    ...req.body,
+  };
+  await student.save();
+  res.json({ message: "Education Updated!" });
+});
+
+exports.deleteeducation = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const filterededu = student.resume.education.filter(
+    (i) => i.id === req.params.eduid
+  );
+  student.resume.education = filterededu;
+  await student.save();
+  res.json({ message: "Education Deleted!" });
+});
+
+//   ---------------------------Job--------------------------
+
+exports.addjob = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  student.resume.jobs.push({ ...req.body, id: uuidv4() });
+  await student.save();
+  res.json({ message: "Job Added!" });
+});
+
+exports.editjob = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const jobIndex = student.resume.jobs.findIndex(
+    (i) => i.id === req.params.jobid
+  );
+  student.resume.jobs[jobIndex] = {
+    ...student.resume.jobs[jobIndex],
+    ...req.body,
+  };
+  await student.save();
+  res.json({ message: "Job Updated!" });
+});
+
+exports.deletejob = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const filteredjob = student.resume.jobs.filter(
+    (i) => i.id !== req.params.jobid
+  );
+  student.resume.jobs = filteredjob;
+  await student.save();
+  res.json({ message: "Job Deleted!" });
+});
+
+// --------------------------- Internships ----------------------------
+
+exports.addintern = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  student.resume.internships.push({ ...req.body, id: uuidv4() });
+  await student.save();
+  res.json({ message: "Internship Added!" });
+});
--- routes/resume.routes.js
+++ routes/resume.routes.js
@@ -1,15 +1,45 @@
 const express = require("express");
 const router = express.Router();
 
-const {resume,addeducation} = require("../controllers/resumeController.js");
+const {
+  resume,
+  addeducation,
+  editeducation,
+  deleteeducation,
+  addjob,
+  editjob,
+  deletejob,
+  addintern,
+} = require("../controllers/resumeController.js");
 const { isAuthenticated } = require("../middleware/auth.js");
 
-
 // GET /
-router.get("/",isAuthenticated,resume)
+router.get("/", isAuthenticated, resume);
+
+// --------------------------Education-------------------
 
 // POST   /add-edu
-router.post("/add-edu",isAuthenticated,addeducation)
+router.post("/add-edu", isAuthenticated, addeducation);
 
+// POST   /edit-edu/:eduid
+router.post("/edit-edu/:eduid", isAuthenticated, editeducation);
+
+// POST   /delete-edu/:eduid
+router.post("/delete-edu/:eduid", isAuthenticated, deleteeducation);
+
+// --------------------------Job-------------------
+
+// POST
+router.post("/add-job", isAuthenticated, addjob);
+
+// POST
+router.post("/edit-job/:jobid", isAuthenticated, editjob);
+
+// POST
+router.post("/delete-job/:jobid", isAuthenticated, deletejob);
+
+// ----------------------------Internships--------------------
+// POST
+router.post("/add-intern", isAuthenticated, addintern);
 
 module.exports = router;
